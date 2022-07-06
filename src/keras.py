fc4f70e31c799742dcd962e8fdee4f0ef0fcc519
kristen-schneider <kristen.schneider@colorado.edu>
2022-07-06 14:43:16 -0700

    stores all 3 (anchor, p, n) in vectors and converts to dataset

--- src/keras.py
+++ src/keras.py
@@ -1,5 +1,6 @@
 import encoded_utils
 import tensorflow as tf
+from tensorflow.keras.applications import resnet
 
 
 encoded_genotype_file = '/home/sdp/genotype-encoding/data/genotype.txt'
@@ -9,7 +10,6 @@ encoded_negative_file = '/home/sdp/genotype-encoding/data/negative.txt'
 def main():
     # counting variants for dimension of vector
     num_variants = encoded_utils.count_variants(encoded_genotype_file)
-    target_shape = (1, num_variants)
     
     # loading vectors (anchor, positive, and negative)
     genotype_vectors = encoded_utils.read_encoded_file(encoded_genotype_file)
@@ -18,13 +18,45 @@ def main():
 
     # counting samples for dimension of dataset
     num_samples = len(genotype_vectors)
+    print(type(num_samples), num_samples)
     print(num_variants, num_samples)
+    target_shape = (num_variants,)
+
+    # making dataset objects from lists
+    genotype_dataset = tf.data.Dataset.from_tensor_slices(genotype_vectors)
+    positive_dataset = tf.data.Dataset.from_tensor_slices(positive_vectors)
+    negative_dataset = tf.data.Dataset.from_tensor_slices(positive_vectors)
 
 
+    #DEBUG
+    print('Genotype vectors\n')
+    for i in genotype_dataset: print(i)
+    #for i in positive_dataset: print(i)
+    #for i in negative_dataset: print(i)
 
-    dataset = tf.data.Dataset.zip((genotype_vectors, positive_vectors, negative_vectors))
+    dataset = tf.data.Dataset.zip((genotype_dataset, positive_dataset, negative_dataset))
     #dataset = dataset.shuffle(buffer_size=1024)
-    #dataset = dataset.map(preprocess_triplets)
+    #dataset = dataset.map(genotype_dataset, positive_dataset, negative_dataset)
+
+    # split dataseet into train and validation
+    train_dataset = dataset.take(round(num_samples * 0.8))
+    val_dataset = dataset.skip(round(num_samples * 0.8))
+
+    train_dataset = train_dataset.batch(32, drop_remainder=False)
+    train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+
+    val_dataset = val_dataset.batch(32, drop_remainder=False)
+    val_dataset = val_dataset.prefetch(tf.data.AUTOTUNE)
+
+    # generate embeddings
+    base_cnn = resnet.ResNet50(
+        weights='imagenet', input_shape=target_shape + (3,), include_top=False
+    )
+    
+    #flatten = layers.Flatten()
+    
+
+
 
 if __name__ == '__main__':
     main()
