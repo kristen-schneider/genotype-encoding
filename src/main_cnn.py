bf65e84972f685789e4ee9205e50767269b82653
kristen-schneider <kristen.schneider@colorado.edu>
2022-07-20 14:58:36 -0700

    loading dataset from file makes separate tf objects for each column

--- src/main_cnn.py
+++ src/main_cnn.py
@@ -6,6 +6,9 @@ from tensorflow import keras
 from tensorflow.keras.layers import Conv1D
 
 CNN_input_file = '../data/fake_input.txt'
+sample1_file = '../data/fake_sample1.txt'
+sample2_file = '../data/fake_sample2.txt'
+distances_file = '../data/fake_distances.txt'
 
 def main():
 
@@ -16,10 +19,12 @@ def main():
     print('Opened file: ', CNN_input_file)
     print('...Number of Variants: ', num_variants)
     print('...Number of Pairwise Distances: ', num_distances)
-
+    print()
+    
     # dataloader
     #   tf.Dataset works from memory arrays
     #   tfrecord binary format reads from file
+    print('Building dataset...')
     dataset = build.build_dataset(CNN_input_file)
     for element in dataset:
         print(element)    
