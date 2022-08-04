3ab9c5e97609dfbbebc71affa1c632df5b9366fd
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-04 11:36:11 -0700

    added read_config files

---
+++ cpp/src/read_config.cpp
@@ -0,0 +1,10 @@
+i#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+map<string, string> get_config_options(string config_file){
+	map <string, string> config_options;
+	return config_options;
+}
