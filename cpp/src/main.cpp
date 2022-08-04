819f6fdefa1d4ae6883e487e6539d07e74d2f1b3
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-04 11:35:41 -0700

    added main

---
+++ cpp/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+
+
+#include "make_encoding_map.h"
+#include "read_config.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+	string configFile = argv[1];   // configuration file will all options
+	map<string, string> config_options;
+
+	config_options = 
+
+	return 0;
+}
