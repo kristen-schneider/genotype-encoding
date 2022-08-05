6f07e6a272c96fdbbaff990ff13dfb9986633434
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-05 12:46:49 -0700

    can read chrm through qual vcf

--- cpp/src/encode_vcf.cpp
+++ cpp/src/encode_vcf.cpp
@@ -9,6 +9,9 @@
 #include <htslib/kstring.h>
 #include <htslib/kseq.h>
 #include <htslib/synced_bcf_reader.h>
+#include <vector>
+
+#include "encode_vcf.h"
 
 using namespace std;
 
@@ -43,8 +46,55 @@ void write_encoded_vcf(string input_vcf_file, map<string, int> encoding_map, str
 		// getting number of samples
 		int num_samples = get_num_samples(vcf_header);
 		cout << "NUM SAMPLES: " << num_samples << endl;
+		// gettting all sequence names
 		const char **sequence_names = get_sequence_names(vcf_header);
+		
+		// initialize and allocate bcf1_t object
+        	bcf1_t *vcf_record = bcf_init();
+        	if (vcf_record == NULL) {
+                	cout << "ERROR: record is empty" << endl;
+        	}
 
+		vector<vector<int>> all_genotype_encodings; // vector of vectors
+		
+		while (bcf_read(vcf_stream, vcf_header, vcf_record)){
+			bcf_unpack(vcf_record, BCF_UN_ALL);
+			bcf_unpack(vcf_record, BCF_UN_INFO);
+
+			// making variable names for each column
+			string chrm = bcf_hdr_id2name(vcf_header, vcf_record->rid);
+			int pos = (unsigned long)vcf_record->pos;
+			string id = vcf_record->d.id;
+			string ref = vcf_record->d.allele[0];
+			string alt = vcf_record->d.allele[1];
+			double_t qual = vcf_record->qual;
+
+			// make chromosome through quality into one vector of strings
+			vector<string> chrm_thru_qual_vector = 
+				{chrm, to_string(pos), id, ref, alt, to_string(qual)};
+			string s;
+			for (const auto &col : chrm_thru_qual_vector) s += "\t" + col;
+			string chrm_thru_qual_string;
+			chrm_thru_qual_string = chrm_thru_qual_string +
+				chrm + "\t" +
+				to_string(pos) + "\t" +
+				id + "\t" +
+				ref + "\t" +
+				alt + "\t" +
+				to_string(qual) + "\t";
+
+			// reading genotypes
+			string s_gt;
+			int *gt = NULL;
+			int ngt = 0;
+			int ngt_arr = 0;
+
+			ngt = bcf_get_genotypes(vcf_header, vcf_record,  &gt, &ngt_arr);
+			
+
+			vector<int> record_encoding;
+		}	
+		
 
 	}
 	
