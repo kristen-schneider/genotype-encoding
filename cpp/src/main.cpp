32ec189a3434e6f33c330ee0e56b17bf4e8745c5
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-07 13:49:34 -0700

    just reading and splitting vcf into many files

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
-
+#include <vector>
 
 #include "encode_vcf.h"
 #include "map_encodings.h"
@@ -28,17 +28,23 @@ int main(int argc, char* argv[]){
 	map<string, int> encoding_map = make_encoding_map(encoding_file);
 	
 	// out_file_names
-	string vcf_out_file = out_dir + "chr14.segment." + config_options["segment_size"] + ".vcf";
-	string encoding_out_file = out_dir + "chr14.segment." + config_options["segment_size"] + ".encoding";
+	//string vcf_out_file = out_dir + "chr14.segment." + config_options["segment_size"] + ".vcf";
+	//string encoding_out_file = out_dir + "chr14.segment." + config_options["segment_size"] + ".encoding";
 
 	// slice vcf into segments
+	//num_variants = num_variants;
+	//num_segments = num_variants / segment_size;
 	cout << "Slicing VCF..." << endl;
-	get_vcf_header(vcf_file, vcf_out_file);
-	slice(vcf_file, segment_size, vcf_out_file);	
+	//get_vcf_header(vcf_file, vcf_out_file);
+	
+	//vector<vector<string>> all_vcf_gt;
+	//all_vcf_gt = read_VCF_data(vcf_file);
+	string base_name = "ALL.chr14";
+	slice(vcf_file, segment_size, base_name, out_dir);	
 
 	// encoded vcf segments 
-	cout << "Encoding VCF..." << endl;
-	write_encoded_vcf(vcf_out_file, encoding_map, encoding_out_file);
+	//cout << "Encoding VCF..." << endl;
+	//write_encoded_vcf(vcf_out_file, encoding_map, encoding_out_file);
 
 
 	return 0;
