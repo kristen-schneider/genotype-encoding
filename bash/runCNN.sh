7cb1dff578d3b813405b6aa86df3c7527d7c8b6d
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-09 12:50:24 -0700

    bash script to test model

--- bash/runCNN.sh
+++ bash/runCNN.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 
-cpp_src_dir="./cpp/src/"
-cpp_include_dir="./cpp/include/"
-cpp_bin_dir="./cpp/bin/"
-config_file="./cpp/config_sample"
+scripts_dir="./python/scripts/"
+sample_IDs_file="../precision-medicine/data/ALL.chr14.sampleIDs"
+sample_encodings_file="./data/segments/ALL.chr14.seg.0.encoding"
+pairwise_distances="./data/segments/ALL.chr14.seg.0.CNN"
+ds_out_dir="./data/datasets/ALL.chr14.seg.0.ds"
+
+#sample_IDs_file="./data/fake_data/fake_IDs"
+#sample_encodings_file="./data/fake_data/fake_encodings"
+#pairwise_distances="./data/fake_data/fake_pairwise_distances"
+#ds_out_dir="./data/fake_data/ds_zipped"
 
 source ~/miniconda3/etc/profile.d/conda.sh 
-conda activate genotype-encoding
+#conda activate genotype-encoding
 
-bin=$cpp_bin_dir"segment"
 
-# COMPILE
-g++ $cpp_src_dir"main.cpp" \
-	$cpp_src_dir"read_config.cpp" \
-	-I $cpp_include_dir \
-	-o $bin
-
-# RUN
-$bin $config_file
+python $scripts_dir/main_cnn.py $sample_IDs_file $sample_encodings_file $pairwise_distances $ds_out_dir
