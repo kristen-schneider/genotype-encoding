6065348d905e23c847e85d2af2cfb91a6b1f45c0
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-10 11:50:39 -0700

    scaling to segment 0 5000 data

--- bash/runCNN.sh
+++ bash/runCNN.sh
@@ -3,11 +3,13 @@
 
 scripts_dir="./python/scripts/"
 sample_IDs_file="../precision-medicine/data/ALL.chr14.sampleIDs"
-sample_encodings_file="./data/segments/seg_1000/ALL.chr14.seg.0.encoding"
-IDs_distances_file="./data/segments/seg_1000/ALL.chr14.seg.0.CNN_IDs"
-encoding_distances_file="./data/segments/seg_1000/ALL.chr14.seg.0.CNN_encodings"
+sample_encodings_file="./data/segments/seg_5000/ALL.chr14.seg.0.encoding"
+IDs_distances_file="./data/segments/seg_5000/ALL.chr14.seg.0.CNN_IDs"
+encoding_distances_file="./data/segments/seg_5000/ALL.chr14.seg.0.CNN_encodings"
 #ds_out_dir="./data/datasets/ALL.chr14.seg.0.ds"
 
+
+## FAKE DATA
 #sample_IDs_file="./data/fake_data/fake_IDs_40x100"
 #sample_encodings_file="./data/fake_data/fake_encodings_40x100"
 #IDs_distances_file="./data/fake_data/fake_ID_distances_40x100"
