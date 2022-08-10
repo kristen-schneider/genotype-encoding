e28e6d0ed72f7c2bcd03c3a9a2825cd38c2e8aec
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-10 09:51:25 -0600

    merging bash run

--- bash/runCNN.sh
+++ bash/runCNN.sh
@@ -7,9 +7,9 @@ sample_encodings_file="./data/segments/ALL.chr14.seg.0.encoding"
 pairwise_distances="./data/segments/ALL.chr14.seg.0.CNN"
 ds_out_dir="./data/datasets/ALL.chr14.seg.0.ds"
 
-#sample_IDs_file="./data/fake_data/fake_IDs"
-#sample_encodings_file="./data/fake_data/fake_encodings"
-#pairwise_distances="./data/fake_data/fake_pairwise_distances"
+#sample_IDs_file="./data/fake_data/fake_IDs_40x100"
+#sample_encodings_file="./data/fake_data/fake_encodings_40x100"
+#pairwise_distances="./data/fake_data/fake_pairwise_distances_40x100"
 #ds_out_dir="./data/fake_data/ds_zipped"
 
 source ~/miniconda3/etc/profile.d/conda.sh 
