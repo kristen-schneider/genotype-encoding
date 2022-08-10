d1f72f00c1cd9f96961c04ccc278eccf7870cdf0
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-10 08:25:08 -0700

    moving local to test sampling with ID distance file

--- bash/runCNN.sh
+++ bash/runCNN.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 scripts_dir="/home/sdp/genotype-encoding/python/scripts/"
-#sample_IDs_file="./data/fake_data/fake_IDs"
-#sample_encodings_file="./data/fake_data/fake_encodings"
-#pairwise_distances="./data/fake_data/fake_CNN"
+sample_IDs_file="./data/fake_data/fake_IDs_40x100"
+sample_encodings_file="./data/fake_data/fake_encodings_40x100"
+pairwise_distances="./data/fake_data/fake_CNN_40x100"
 #ds_out_dir="./data/fake_data/ds_zipped"
 
-sample_IDs_file="../precision-medicine/data/ALL.chr14.sampleIDs"
-sample_encodings_file="./data/segments/seg_1000/ALL.chr14.seg.0.encoding"
-pairwise_distances="./data/segments/seg_1000/ALL.chr14.seg.0.CNN"
+#sample_IDs_file="../precision-medicine/data/ALL.chr14.sampleIDs"
+#sample_encodings_file="./data/segments/seg_5000/ALL.chr14.seg.0.encoding"
+#pairwise_distances="./data/segments/seg_5000/ALL.chr14.seg.0.CNN"
 #ds_out_dir="./data/datasets/ALL.chr14.seg.0.ds"
 
 
