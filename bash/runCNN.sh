5b98d85c7ed18cd7ee15032aa50f6cfe1481a12c
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-10 09:17:32 -0700

    sampling on seg 1000 good

--- bash/runCNN.sh
+++ bash/runCNN.sh
@@ -3,17 +3,19 @@
 
 scripts_dir="./python/scripts/"
 sample_IDs_file="../precision-medicine/data/ALL.chr14.sampleIDs"
-sample_encodings_file="./data/segments/ALL.chr14.seg.0.encoding"
-pairwise_distances="./data/segments/ALL.chr14.seg.0.CNN"
-ds_out_dir="./data/datasets/ALL.chr14.seg.0.ds"
+sample_encodings_file="./data/segments/seg_1000/ALL.chr14.seg.0.encoding"
+IDs_distances_file="./data/segments/seg_1000/ALL.chr14.seg.0.CNN_IDs"
+encoding_distances_file="./data/segments/seg_1000/ALL.chr14.seg.0.CNN_encodings"
+#ds_out_dir="./data/datasets/ALL.chr14.seg.0.ds"
 
 #sample_IDs_file="./data/fake_data/fake_IDs_40x100"
 #sample_encodings_file="./data/fake_data/fake_encodings_40x100"
-#pairwise_distances="./data/fake_data/fake_pairwise_distances_40x100"
+#IDs_distances_file="./data/fake_data/fake_ID_distances_40x100"
+#encoding_distances_file="./data/fake_data/fake_encoding_distances_40x100"
 #ds_out_dir="./data/fake_data/ds_zipped"
 
 source ~/miniconda3/etc/profile.d/conda.sh 
 #conda activate genotype-encoding
 
 
-python $scripts_dir/main_cnn.py $sample_IDs_file $sample_encodings_file $pairwise_distances $ds_out_dir
+python $scripts_dir/main_cnn.py $sample_IDs_file $sample_encodings_file $IDs_distances_file $encoding_distances_file
