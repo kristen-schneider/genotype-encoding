456ed0ab6feb8785b5d83297d6413b65df7306d6
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-09 17:57:23 -0700

    moving fake data names

--- bash/runCNN.sh
+++ bash/runCNN.sh
@@ -4,12 +4,12 @@
 scripts_dir="/home/sdp/genotype-encoding/python/scripts/"
 sample_IDs_file="./data/fake_data/fake_IDs"
 sample_encodings_file="./data/fake_data/fake_encodings"
-pairwise_distances="./data/fake_data/fake_pairwise_distances"
-ds_out_dir="./data/fake_data/ds_zipped"
+pairwise_distances="./data/fake_data/fake_CNN"
+#ds_out_dir="./data/fake_data/ds_zipped"
 
 #sample_IDs_file="../precision-medicine/data/ALL.chr14.sampleIDs"
-#sample_encodings_file="./data/segments/ALL.chr14.seg.0.encoding"
-#pairwise_distances="./data/segments/ALL.chr14.seg.0.CNN"
+#sample_encodings_file="./data/segments/seg_1000/ALL.chr14.seg.0.encoding"
+#pairwise_distances="./data/segments/seg_1000/ALL.chr14.seg.0.CNN"
 #ds_out_dir="./data/datasets/ALL.chr14.seg.0.ds"
 
 
